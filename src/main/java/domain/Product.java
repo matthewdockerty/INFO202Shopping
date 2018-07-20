e6bf5e943b96ba107edc0d285255094f35d37fab
Matthew Dockerty <dockertyml@gmail.com>
2018-07-20 23:27:49 +1200

    Added Product domain class. References #1.

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,78 @@
+package domain;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Matthew
+ */
+public class Product {
+
+    private int productID;
+    private String name, description, category;
+    private BigDecimal listPrice;
+    private Integer quantityInStock;
+
+    public Product(int productID, String name, String description, String category, BigDecimal listPrice, Integer quantityInStock) {
+        this.productID = productID;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.listPrice = listPrice;
+        this.quantityInStock = quantityInStock;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(BigDecimal listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", name=" + name + '}';
+    }
+
+}
