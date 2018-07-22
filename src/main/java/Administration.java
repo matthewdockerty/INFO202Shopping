fa7adebdb6957bcc182c23f7b2a151a3ef98f9ab
Matthew Dockerty <dockertyml@gmail.com>
2018-07-22 15:27:18 +1200

    Created and displayed instance of main menu frame in main method. References #4.

--- src/main/java/Administration.java
+++ src/main/java/Administration.java
@@ -1,3 +1,6 @@
+
+import gui.FrameMainMenu;
+
 /**
  *
  * @author docma436
@@ -5,7 +8,9 @@
 public class Administration {
 
     public static void main(String[] args) {
-        
+        FrameMainMenu frame = new FrameMainMenu();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
     
 }
