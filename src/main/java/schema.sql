f9c40aedddf32ec4671b2bfdc86259a5bac09ec4
Matthew Dockerty <dockertyml@gmail.com>
2018-08-15 14:42:49 +1200

    Add constraints to Product table schema. Closes #29

--- src/main/java/schema.sql
+++ src/main/java/schema.sql
@@ -5,11 +5,12 @@
 
 CREATE TABLE Product (
     Product_ID VARCHAR(6) NOT NULL,
-    Name VARCHAR(32) NOT NULL,
-    Description VARCHAR(255) NOT NULL,
-    Category VARCHAR(32) NOT NULL,
-    List_Price Decimal(19, 2) NOT NULL,
-    Quantity_In_Stock Integer NOT NULL,
+    Name VARCHAR(32) NOT NULL CHECK (LENGTH(Name) >= 2),
+    Description VARCHAR(255) NOT NULL CHECK (LENGTH(Description) >= 4),
+    Category VARCHAR(32) NOT NULL CHECK (LENGTH(Category) > 0),
+    List_Price Decimal(19, 2) NOT NULL CHECK (List_Price >= 0),
+    Quantity_In_Stock Integer NOT NULL CHECK (Quantity_In_Stock >= 0),
     
-    CONSTRAINT Product_PK PRIMARY KEY (Product_ID)
+    CONSTRAINT Product_PK PRIMARY KEY (Product_ID),
+    CONSTRAINT Product_ID CHECK(REGEXP_LIKE(Product_ID, '[A-Z]{2}[0-9]{4}'))
 );
