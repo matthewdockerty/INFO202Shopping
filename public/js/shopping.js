9aa19333d03751358df0ebfad7441480eb8c52c4
Matthew Dockerty <dockertyml@gmail.com>
2018-09-10 16:21:50 +1200

    Add category filters to view products webpage. References #42.

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -6,8 +6,23 @@ module.factory('productDAO', function ($resource) {
     return $resource('/api/products/:id');
 });
 
-module.controller('ProductController', function (productDAO) {
+module.factory('categoryDAO', function ($resource) {
+    return $resource('/api/categories/:category');
+});
+
+module.controller('ProductController', function (productDAO, categoryDAO) {
     // load the products
     this.products = productDAO.query();
+    // load the categories
+    this.categories = categoryDAO.query();
+
+    // click handler for the category filter buttons
+    this.selectCategory = function (selectedCat) {
+        this.products = categoryDAO.query({"category": selectedCat});
+    };
+    
+    this.selectAll = function () {
+        this.products = productDAO.query();
+    }
 });
 
