487a1e7ab0a4185e9857be7f0477775aec415afe
Matthew Dockerty <dockertyml@gmail.com>
2018-09-27 12:49:04 +1200

    Start work on lab11 email. WIP. References #53.

--- src/main/java/web/SaleModule.java
+++ src/main/java/web/SaleModule.java
@@ -3,6 +3,13 @@ package web;
 import dao.DAOException;
 import dao.SaleDAO;
 import domain.Sale;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.jooby.Jooby;
 import org.jooby.Status;
 
@@ -20,13 +27,28 @@ public class SaleModule extends Jooby {
             try {
                 saleDAO.save(sale);
                 rsp.status(Status.CREATED);
+
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        Email email = new SimpleEmail();
+                        email.setHostName("localhost");
+                        email.setSmtpPort(2525);
+                        email.setFrom("orders@doohickiesandwidgets.com");
+                        email.setSubject("Your Order");
+                        email.setMsg("Thank you for your order.");
+                        email.addTo(sale.getCustomer().getEmailAddress());
+                        email.send();
+                    } catch (EmailException e) {
+                        System.err.println("Unable to send email: " + e.getMessage());
+                    }
+                });
+
             } catch (DAOException e) {
                 rsp.status(Status.FORBIDDEN);
                 rsp.send(e.getMessage());
             }
         });
-        
-        
+
     }
 
 }
